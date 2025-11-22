48058c97c42956cfafea15f3142fba280ce51438
Daniel <30800499+daniel-granger@users.noreply.github.com>
2025-11-22 20:59:56 +0000

    [1] feat: Create blog models
    - Created BlogPost, Comment models for Posts/Models
    - Created BlogPostRoot as Deserialisation entry point.
    - Moved Blog-Post JSON to appropriate location (/Data)

---
+++ src/DeveloperAssessment.Web/Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿namespace DeveloperAssessment.Web.Models
+{
+    public sealed class BlogPost
+    {
+        public int? Id { get; set; }
+        public DateTime? Date { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string HtmlContent { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
---
+++ src/DeveloperAssessment.Web/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace DeveloperAssessment.Web.Models
+{
+    public class Comment
+    {
+        public string Name { get; set; }
+        public DateTime? Date { get; set; }
+        public string EmailAddress { get; set; }
+        public string Message { get; set; }
+    }
+}
