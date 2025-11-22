da3e614a31859100b1e28fb51227db69f9f6c0e9
Daniel <30800499+daniel-granger@users.noreply.github.com>
2025-11-22 21:54:59 +0000

    feat: Set Explicit JSON Property Name
    - Clearer mapping to JSON values
    - Variable names can change without need for JSON to change.

--- src/DeveloperAssessment.Web/Models/BlogPost.cs
+++ src/DeveloperAssessment.Web/Models/BlogPost.cs
@@ -1,12 +1,20 @@
-﻿namespace DeveloperAssessment.Web.Models
+﻿using System.Text.Json.Serialization;
+
+namespace DeveloperAssessment.Web.Models
 {
     public sealed class BlogPost
     {
+        [JsonPropertyName("id")]
         public int? Id { get; set; }
+        [JsonPropertyName("date")]
         public DateTime? Date { get; set; }
+        [JsonPropertyName("title")]
         public string Title { get; set; }
+        [JsonPropertyName("image")]
         public string Image { get; set; }
+        [JsonPropertyName("htmlContent")]
         public string HtmlContent { get; set; }
+        [JsonPropertyName("comments")]
         public List<Comment> Comments { get; set; }
     }
 }
--- src/DeveloperAssessment.Web/Models/Comment.cs
+++ src/DeveloperAssessment.Web/Models/Comment.cs
@@ -1,10 +1,16 @@
-﻿namespace DeveloperAssessment.Web.Models
+﻿using System.Text.Json.Serialization;
+
+namespace DeveloperAssessment.Web.Models
 {
     public class Comment
     {
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+        [JsonPropertyName("date")]
         public DateTime? Date { get; set; }
+        [JsonPropertyName("emailAddress")]
         public string EmailAddress { get; set; }
+        [JsonPropertyName("message")]
         public string Message { get; set; }
     }
 }
